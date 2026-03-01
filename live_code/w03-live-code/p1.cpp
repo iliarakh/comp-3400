b51b1f4d3ec572ec6e520eb5c260dc177ddade2f
Ilia Rakhoutine <08rakhoutine@gmail.com>
2026-03-01 14:07:47 -0500

    feat: live code examples for reference

---
+++ live_code/w03-live-code/p1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class foo2
+{
+private:
+  int i_;
+
+public:
+  foo2(int i) : i_(5) { }
+};
+
+class foo
+{
+public:
+  foo(int i) 
+  {
+    i = 5;
+  }
+
+private:
+  int i_;
+};
+
+int main()
+{
+}
